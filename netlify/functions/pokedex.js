c4bd0c61d6ea0c2da7c2153910fb441e57d2911c
Dawidx3p <maildawcio@gmail.com>
2022-02-17 00:10:06 +0100

    reapairing

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 exports.handler = async function(){
-    POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
     const response = await fetch(POKE_API);
     const data = await response.json();
     return{
