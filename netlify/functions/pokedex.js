6137b2adbe0320d9b0c9a7ac2834ea755a2a1a64
Dawidx3p <maildawcio@gmail.com>
2022-02-17 00:07:32 +0100

    reapairing

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,11 +1,8 @@
-import fetch from 'node-fetch';
-
 exports.handler = async function(){
-    POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
-    response = await fetch(POKE_API);
-    data = await response.json();
     return{
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+            message: 'Hello World!'
+        })
     }
 }
