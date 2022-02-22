7c0d53af610493fa73d89c19ed0fd5b6112f29cf
Dawidx3p <maildawcio@gmail.com>
2022-02-22 20:11:05 +0100

    trying to learn fauna

---
+++ netlify/functions/faunadb.js
@@ -0,0 +1,25 @@
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+console.log(process.env.FAUNADB_ADMIN_SECRET);
+
+exports.handler = (event, context) => {
+  console.log('creating test data')
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_ADMIN_SECRET
+  }) 
+  return client.query(
+    q.Create(
+      q.Collection('test'),
+      { data: { testField: 'testValue' } }
+    )
+  ).catch((error) => {
+      console.log('error', error)
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
+}
